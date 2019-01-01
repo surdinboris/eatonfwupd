0edc45900b6604d42d6fa42a58b3dbbff978b3e1
borrik <borrik@live.ru>
2019-01-01 07:48:29 +0200

    simple eaton fw updater with automatic card hw ver detection

--- fupd.py
+++ fupd.py
@@ -7,11 +7,9 @@ import re
 import os
 from shutil import copyfile
 
-debug = True
-buff=io.BytesIO(b'')
+debug = False
+buff= io.BytesIO(b'')
 
-
-#sio = io.TextIOWrapper(ser,  newline='\r')
 def serial_ports():
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
@@ -31,56 +29,60 @@ def serial_ports():
             pass
     return result
 
-def inwait(func): #decorator function for monitoring
-    def wrapper(*args,**kwargs):
-        func(*args, **kwargs)
-        if debug == True and (ser.inWaiting() > 0):
-            # if incoming bytes are waiting to be read from the serial input buffer
-            data_str = ser.read(ser.inWaiting()).decode('ascii')
-            buff.write(ser.read(ser.inWaiting()))
-            print(data_str, end='')
-
-    return wrapper
 
 def sendcom(cmd=''):
     ser.write(cmd.encode())
     ser.write("\r\n".encode())
 
-# def login():
-#     for e in range(2):
-#         sendcom()
-#     ser.read_until(b"Enter password to activate Maintenance Menu :")
-#     sendcom(b'admin')
-#     ser.read_until(b"0 : Exit")
-#     print('logged in')
-
-def read_until(val=None):
+def read_until(val,inval=None):
     while True:
         line = str(ser.readline().decode('ascii'))
         if len(line) > 1:
-            print(line)
+            if debug:
+                print('>',line)
+        if inval:
+            invrev = re.search(inval, line)
+        if inval and invrev:
+            raise ConnectionError("Network card does not retrieved IP via DHCP and asks for manual IP address, please check your DHCP server configuration")
         rev = re.search(val, line)
         if val and rev:
             return rev
 
 def write(comm):
-    print('Writing {}'.format(comm))
-    ser.write(comm.encode())
-    time.sleep(0.5)
-    ser.write('\r\n'.encode())
+    if len(comm) == 1:
+        if debug:
+            print('Writing {}'.format(comm.encode('ascii')))
+        ser.write(comm.encode('ascii'))
+    else:
+        if debug:
+            print('Writing {}'.format(comm.encode('ascii')))
+        for sym in comm:
+            ser.write(sym.encode('ascii'))
+            time.sleep(0.1)
+
+    time.sleep(1)
+    ser.write('\r'.encode('ascii'))
+
+def searchv(val):
+    while True:
+        line = str(ser.readline().decode('UTF-8'))
+        rev = re.search('{} (\w+)'.format(val), line)
+        if debug:
+            print(line)
+        if rev:
+            return rev[1]
 
 def upgrade():
-    ser.flushInput()
+    print('\n')
+    input('Please connect bbu to COM port and network, plug in power cord and press any key...')
+    print('\n')
     #Booting - searching for revision
-    while True:
-        line = str(ser.readline().decode('ascii'))
-        rev = re.search('Kitting revision : (\w+)', line)
-        print(line)
-        if rev:
-            break
-    print('~~~Network card found, revision {}~~~\n'.format(rev[1]))
 
-    if rev[1] == 'LA':
+    rev = searchv('Kitting revision :')
+    sn = searchv('Serial number :')
+    print('  > Network card {} found, revision {}'.format(sn, rev))
+
+    if rev == 'LA':
         #put filename on tftp server for new revision NMC_EATON_JB_rp.bin
         copyfile(os.path.join(os.getcwd(), 'NMC_EATON_JB_rp.bin'),os.path.join(os.getcwd(),'TFTP-Root','image.bin'))
     else:
@@ -90,31 +92,31 @@ def upgrade():
     #Waiting for upgrade mode
     read_until("To force the upgrade mode, type 'y', then press ENTER")
     write('y')
-    print("~~~Wait 3 min until boot...~~~")
-    read_until("Set the IP address :")
-    write('192.168.1.2')
-    read_until("Set the subnet mask address :")
-    write('255.255.255.0')
-    read_until("Set the gateway address :")
-    write('0.0.0.0')
-    read_until('Set the TFTP server IP address :')
+    print("  > Wait 1 min until boot...")
+    # read_until("Set the IP address :")
+    # write('192.168.1.2')
+    # read_until("Set the subnet mask address :")
+    # write('255.255.255.0')
+    # read_until("Set the gateway address :")
+    # write('0.0.0.0')
+    read_until('Set the TFTP server IP address :',"Set the IP address :")
+    print("  > Setting TFTP server IP address....")
     write('192.168.1.3')
-    read_until()
-
-
+    print("  > Working. DO NOT DISCONNECT BBU! Please wait...")
+    read_until('Flashing done')
+    print('  > Flashing done sucessfully.')
+    print('~'*300)
+    upgrade()
 
 if __name__ == "__main__":
     for file in os.listdir(os.path.join(os.getcwd(),'TFTP-Root')):
         os.remove(os.path.join(os.getcwd(),'TFTP-Root', file))
     port=serial_ports()[0]
     ser = serial.Serial(port,
-                        timeout=30,
+                        timeout=10,
                         baudrate=9600,
                         xonxoff=0,
                         stopbits=1,
                         parity=serial.PARITY_NONE,
                         bytesize=8)
-    print('Please connect bbu to {}, ethernet, plug in power cord and press Enter...'.format(port))
-    input()
     upgrade()
-    #command('tcpip')
